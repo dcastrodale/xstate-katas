db423917e974a1bb12483838b533054e9cb3c8b8
David Castrodale <dcastrodale@gmail.com>
2024-08-04 21:16:34 +0100

    Create outline of katas

--- app/root.tsx
+++ app/root.tsx
@@ -2,6 +2,7 @@ import { Links, Meta, Outlet, Scripts } from '@remix-run/react';
 
 import type { LinksFunction } from '@remix-run/node';
 import stylesheet from '../tailwind.css?url';
+import { ReactNode } from 'react';
 
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: stylesheet },
@@ -16,10 +17,74 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <FullScreenLayout>
+          <NavMenu />
 
+          <Content>
+            <Outlet />
+          </Content>
+        </FullScreenLayout>
         <Scripts />
       </body>
     </html>
   );
+}
+
+function FullScreenLayout({ children }: { children?: ReactNode }): JSX.Element {
+  return (
+    <section className="w-100 h-screen bg-slate-50 flex">{children}</section>
+  );
+}
+
+function NavMenu(): JSX.Element {
+  return (
+    <nav className="w-1/5 h-100 p-3 bg-slate-800 text-slate-50">
+      <h1 className="text-xl mb-3">Katas</h1>
+
+      <ul>
+        <li>
+          <h3 className="font-bold text-lg">0. State Machine Basics</h3>
+          <ul className="p-3">
+            <li>0-0: The Stoplight</li>
+            <li>0-1: The Automated Stoplight</li>
+            <li>0-2: The Tape Recorder</li>
+            <li>0-3: The Random Number Generator</li>
+            <li>0-4: The Random Delay Button</li>
+          </ul>
+        </li>
+
+        <li>
+          <h3 className="font-bold text-lg">1. Requests and Authentication</h3>
+          <ul className="p-3">
+            <li>1-0: A Simple Request</li>
+            <li>1-1: A Flaky Request</li>
+            <li>1-2: Authentication</li>
+            <li>1-0: An Authenticated Request</li>
+          </ul>
+        </li>
+
+        <li>
+          <h3 className="font-bold text-lg">2. WebsSockets</h3>
+          <ul className="p-3">
+            <li>2-0: A Simple WebSocket Connection</li>
+            <li>2-1: A Complex WebSocket</li>
+            <li>2-2: An Event Subscription</li>
+          </ul>
+        </li>
+
+        <li>
+          <h3 className="font-bold text-lg">3. Business Logic</h3>
+          <ul className="p-3">
+            <li>3-0: Logging In</li>
+            <li>3-1: Navigation</li>
+            <li>3-2: Basket Building</li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+function Content({ children }: { children?: ReactNode }): JSX.Element {
+  return <section className="p-3 bg-slate-50">{children}</section>;
 }
