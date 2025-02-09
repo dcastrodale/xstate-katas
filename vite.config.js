5f3e7648745fc2f5eea3ecf9abef9fb4f7017e6e
David Castrodale <dcastrodale@gmail.com>
2025-02-09 13:52:15 +0000

    Upgrade dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { vitePlugin as remix } from '@remix-run/dev';
+import tailwindcss from '@tailwindcss/vite';
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [remix(), tailwindcss()],
 });
